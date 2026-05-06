dca2f1c7c02d74c850c6cd444d074f9314160918
AbdulRahim-D <abdulrahimcc4516@gmail.com>
2026-05-06 16:17:34 +0530

    getClassById endpoint

--- Backend/routes/classRoutesAPI.js
+++ Backend/routes/classRoutesAPI.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const { isAuthUser, isTeacher } = require("../middlewares/authmiddleware");
 const { join } = require("path");
 const { count } = require("console");
-const { createClass, joinClass, getAllClasses } = require("../controllers/Class.controller");
+const { createClass, joinClass, getAllClasses, getClassById } = require("../controllers/Class.controller");
+const Class = require("../models/Class.model");
 
 router.post("/create", isAuthUser, isTeacher,createClass)
 
 router.patch("/join", isAuthUser, joinClass)
 
 router.get("/", isAuthUser, getAllClasses)
+
+router.get("/:id",isAuthUser,getClassById)
 
 module.exports=router
