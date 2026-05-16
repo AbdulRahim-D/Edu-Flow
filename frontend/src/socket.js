406fa1bbd8818ef406a09d7bdf6e70a6cc47901a
Abdul Rahim Dudekula <abdulrahimcc4516@gmail.com>
2026-05-16 14:22:11 +0530

    Change socket connection URL to production server

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:6142");
+// export const socket = io("http://localhost:6142");
+export const socket = io("https://edu-flow-be.onrender.com");
 
 //use to give a single pipeline or a same connection for every dynamic update 
